ec8ec6f8ed56d1246a7720d87186887fe0b52e87
RickyL-2000 <15305813731@163.com>
2020-06-08 22:30:32 +0800

    很多细节bug...不过总算完成了

--- Algorithm/Data_Structure(Summer2020)/pp02.cpp
+++ Algorithm/Data_Structure(Summer2020)/pp02.cpp
@@ -10,22 +10,39 @@ int main() {
         scanf("%d", &array[i]);
     }
     int sum = 0, ans = 0;
-    int left = 0, right = 0, temp_left = 0;
-    for (int i = 0; i < k; i++) {
+    // int left = 0, right = 0, temp_left = 0;
+    // for (int i = 0; i < k; i++) {
+    //     sum += array[i];
+    //     if (sum < 0) {
+    //         sum = 0;
+    //         temp_left = i+1;
+    //     }
+    //     // sum = sum < 0 ? 0 : sum;
+    //     //ans = ans < sum ? sum : ans;
+    //     if (ans < sum) {
+    //         ans = sum;
+    //         left = temp_left;
+    //         right = i;
+    //     }
+    // }
+    int left = k, right = k, temp_right = k-1;
+    bool isNegative;
+    for (int i = k-1; i >= 0; i--) {
         sum += array[i];
+        isNegative = false;
         if (sum < 0) {
+            isNegative = true;
             sum = 0;
-            temp_left = i+1;
+            temp_right = i-1;
         }
-        // sum = sum < 0 ? 0 : sum;
-        //ans = ans < sum ? sum : ans;
-        if (ans < sum) {
+        if (ans <= sum && !isNegative) {
             ans = sum;
-            left = temp_left;
-            right = i;
+            right = temp_right;
+            left = i;
         }
     }
-    if (ans == 0) printf("0 %d %d", array[0], array[k-1]);
+    while (right > left && array[right] == 0) right--;
+    if (ans == 0 && (left == k || right == k)) printf("0 %d %d", array[0], array[k-1]);
     else printf("%d %d %d", ans, array[left], array[right]);
     return 0;
 }
